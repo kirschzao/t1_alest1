a72c0b9f66f35116de9ed48c4312103d0c08cf5d
Bernardo <bekirsch123@gmail.com>
2024-09-23 18:57:47 -0300

    Implementando o O(1)

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,34 +1,33 @@
-
-
 public class ArrayQueue implements QueueTAD {
-    private int[] array = new int[8];
-    private int index = 0;
+    private int[] array;
+    private int start;
+    private int end;
+    private int index;
 
     public ArrayQueue(){
-        
+        array = new int[8];
+        start = 0;
+        end = 0;
+        index = 0;
     }
 
     /**
      * Adiciona um elemento no final da fila.
      * @param element
      */
-    public void enqueue(int element){
-        
-        if(index + 1 < array.length){
-            array[index] = element;
-            System.out.println("Inserindo " + element + " no index " + index);
-            
-            index++;
-        }else{
-            int[] temp = new int[array.length*2];
-            temp = array;
-            temp[index] = element;
-            System.out.println("Recriando temp array " + element + " no index " + index);   
-
-
-            index++;
+    public void enqueue(int element) {
+        if (index == array.length) {
+            int[] temp = new int[array.length * 2];
+            for (int i = 0; i < index; i++) {
+                temp[i] = array[(start + i) % array.length];
+            }
             array = temp;
+            start = 0;
+            end = index;
         }
+        array[end] = element;
+        end = (end + 1) % array.length;
+        index++;
     }
 
     /**
@@ -36,34 +35,42 @@ public class ArrayQueue implements QueueTAD {
      * @return O elemento que estava no início da fila.
      */
     public int dequeue() {
-        return 0;
+        if (isEmpty()) {
+            throw new IllegalStateException("Fila cheia");
+        }
+        int element = array[start];
+        start = (start + 1) % array.length;
+        index--;
+        return element;
     }
 
     /**
      * @return O tamanho da fila.
      */
     public int size() {
-        return 0;
+        return this.index;
     }
 
     /**
      * @return true se a fila estiver vazia, false caso contrário.
      */
     public boolean isEmpty() {
-        return false;
+        return index == 0;
     }
 
     /**
      * Remove todos elementos da fila.
      */
-    public void clear() {}
+    public void clear() {
+        
+    }
 
     /**
      * Retorna o elemento no início da fila, sem removê-lo.
      * @return O primeiro elemento da fila.
      */
     public int head() {
-        return 1;
+        return array[0];
     }
 
 }
--- Main.java
+++ Main.java
@@ -5,12 +5,14 @@ public class Main{
 
         arrayQueue.enqueue(5);
         arrayQueue.enqueue(6);
-        arrayQueue.enqueue(7);
-        arrayQueue.enqueue(8);
-        arrayQueue.enqueue(9);
-        arrayQueue.enqueue(10);
-        arrayQueue.enqueue(11);
-        arrayQueue.enqueue(12);
+        arrayQueue.dequeue();
+        // arrayQueue.enqueue(6);
+        // arrayQueue.enqueue(7);
+        // arrayQueue.enqueue(8);
+        // arrayQueue.enqueue(9);
+        // arrayQueue.enqueue(10);
+        // arrayQueue.enqueue(11);
+        // arrayQueue.enqueue(12);
 
     }
 
